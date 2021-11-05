42f9287bbac32dc402f00f7cb3b379b5b4172831
Danilo Herrera <daniloherrera1702@gmail.com>
2021-11-04 23:58:00 -0500

    Try to modularize the code

--- handman_game.py
+++ handman_game.py
@@ -1,8 +1,10 @@
 import random
 import os
 
+
 def run():
     os.system("clear")
+    # Select a word
     aleatory_number = random.randint(1, 172)
     words = []
     with open("./data.txt", "r", encoding="utf-8") as f:
@@ -11,36 +13,40 @@ def run():
 
     select_word = words[aleatory_number]
     select_word = select_word.replace('\n', '')
+    #become and counting letters of the word
     letter_by_letter = list(select_word)
     count_letter = 0
     for letter in letter_by_letter:
-        count_letter = count_letter + 1 
+        count_letter += 1 
 
-
+    #The list of the user
     show = []
     for i in range(0, count_letter):
         show.append("_")
 
     count_time = 0
 
-    while count_letter > count_time:
+    # Big loop for a game
+    # while count_letter > count_time:
+    while show != letter_by_letter:
         user = str(input("\n Escribe una letra: "))
         count = -1
         for i in select_word:
-            count = count + 1
+            count += 1
             if i == user:
                 show[count] = user
-                count_time = count_time + 1
+                count_time += 1
             else:
                 continue
 
         # print(show)
-        print(select_word)
+        # print(select_word)
         # print("Contador de letras", count_letter)
         for i in range(0, count_letter):
             print(show[i], end=" ")
         print("\n Tu letra ateriror fue ", user)
         # print("contador de tiempos", count_time)
+    os.system("clear")
     print("Ganaste Felicidades :)")
     
     
